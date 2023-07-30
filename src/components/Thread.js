cf5ac65b7ff82aa721daf07dc484ff4e453da7c2
RIKI PLAZA <rikiplaza1@gmail.com>
2023-07-30 02:45:43 -0400

    icons handed.

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -1,10 +1,30 @@
 const Thread = () => {
-    return (
-      <article className="feed-card">
-        
-      </article>
-    );
-  }
+  return (
+  <article className="feed-card">
+    <div className="text-container">
+      <div>
+            <div className="img-container">
+              <img src="" alt="profile avatar"/>
+            </div>  
+            <div>
+              <p><strong>handle</strong></p>
+              <p>text</p>
+            </div>
+          <div>
+          <p>time</p>
+        </div>
+        <div className="icons">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M17.867 3.493l4.133 3.444v5.127l-10 8.333-10-8.334v-5.126l4.133-3.444 5.867 3.911 5.867-3.911zm.133-2.493l-6 4-6-4-6 5v7l12 10 12-10v-7l-6-5z"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7 11c-.828 0-1.5-.671-1.5-1.5s.672-1.5 1.5-1.5c.829 0 1.5.671 1.5 1.5s-.671 1.5-1.5 1.5zm5 0c-.828 0-1.5-.671-1.5-1.5s.672-1.5 1.5-1.5c.829 0 1.5.671 1.5 1.5s-.671 1.5-1.5 1.5zm5 0c-.828 0-1.5-.671-1.5-1.5s.672-1.5 1.5-1.5c.829 0 1.5.671 1.5 1.5s-.671 1.5-1.5 1.5zm5-8v13h-11.643l-4.357 3.105v-3.105h-4v-13h20zm2-2h-24v16.981h4v5.019l7-5.019h13v-16.981z"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 10v7h10.797l1.594 2h-14.391v-9h-3l4-5 4 5h-3zm14 4v-7h-10.797l-1.594-2h14.391v9h3l-4 5-4-5h3z"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 10v7h10.797l1.594 2h-14.391v-9h-3l4-5 4 5h-3zm14 4v-7h-10.797l-1.594-2h14.391v9h3l-4 5-4-5h3z"/></svg>
+        </div>
+      </div>
+
+    </div>
+  </article>
+  );
+}
   
-  export default Thread;
+export default Thread;
   
